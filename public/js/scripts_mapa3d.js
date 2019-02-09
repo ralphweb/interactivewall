09a0ebd642f58b19c31a91d918a90e50458adf0b
Rodolfo Inostroza Carvajal <ralphweb@gmail.com>
2019-02-09 13:28:39 -0300

    mapa 3d

--- public/js/scripts_mapa3d.js
+++ public/js/scripts_mapa3d.js
@@ -59,8 +59,8 @@ function panTo(coords) {
     scoords[1] = coords[1]>0?coords[1]-32:coords[1]-32;
   } else {
     if(markerHasMedia[index]=="image") {
-      scoords[0] = coords[0]>0?coords[0]-24:coords[0]-24;
-      scoords[1] = coords[1]>0?coords[1]-2:coords[1]-2;
+      scoords[0] = coords[0]>0?coords[0]-54:coords[0]-54;
+      scoords[1] = coords[1]>0?coords[1]-52:coords[1]-52;
     }
     if(markerHasMedia[index]=="video") {
       scoords[0] = coords[0]>0?coords[0]-24:coords[0]-24;
@@ -73,7 +73,7 @@ function panTo(coords) {
 
 $(function() {
   map = WE.map('map', {
-    center: [36.057944835, -212.18688965],
+    center: [86.057944835, -112.18688965],
     zoom: 4,
     dragging: true,
     scrollWheelZoom: true
