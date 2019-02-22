8eb587198d385d5045e46382ffcdfc9e3e9e3a7c
Rodolfo Inostroza Carvajal <ralphweb@gmail.com>
2019-02-22 13:48:28 -0300

    sh logo 4 videowall

--- public/js/scripts_mapa3d.js
+++ public/js/scripts_mapa3d.js
@@ -46,8 +46,8 @@ function panTo(coords) {
   coords[1] = parseFloat(coords[1]);
   let scoords = [];
   if(!markerHasMedia[index]) {
-    coords[0] = coords[0]+22;
-    coords[1] = coords[1]+1;
+    coords[0] = coords[0]+20;
+    coords[1] = coords[1]+3;
     scoords[0] = coords[0]>0?coords[0]-24:coords[0]-24;
     scoords[1] = coords[1]>0?coords[1]-32:coords[1]-32;
   } else {
